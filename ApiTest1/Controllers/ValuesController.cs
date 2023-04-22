505d9d8c2bd4bc7bb5b6ab650c209fc2a627b912
ApogeeX <emil.palczynski@gmail.com>
2023-04-22 14:40:30 +0200

    PUT added

--- ApiTest1/Controllers/ValuesController.cs
+++ ApiTest1/Controllers/ValuesController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiTest1.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -19,6 +19,30 @@ namespace ApiTest1.Controllers
         public string Get(int id)
         {
             return "value" + id;
+        }
+
+        // POST /api/Values
+
+        [HttpPost]
+
+        //public string Post(string value, int value2)
+        public string Post([FromBody] string value)
+        {
+            return $"POST: {value}";
+        }
+
+        // DELETE /api/Values/1
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Do usuniecia: {id}";
+        }
+
+        //PUT  /api/Values/1
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody] string value)
+        {
+            return $"PUT: {id} , {value}";
         }
     }
 }
