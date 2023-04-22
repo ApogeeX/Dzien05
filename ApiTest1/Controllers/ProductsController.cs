c51cd2408987a5bf3b25f8a13782d38a73ab4474
ApogeeX <emil.palczynski@gmail.com>
2023-04-22 16:28:59 +0200

    mySqlWebAPI

---
+++ ApiTest1/Controllers/ProductsController.cs
@@ -0,0 +1,74 @@
+﻿using ApiTest1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTest1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly List<Products> products = new List<Products>
+        {
+            new Products
+            {
+                Id = 1, Name = "Mleko 3.2", Description = "Świeże mleko", Price = 4.49M
+            },
+            new Products
+            {
+                Id = 2, Name = "Coca cola 2l", Description = "Coca-cola 2L", Price = 6.49M
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Products>> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Products> Get(int id)
+        {
+            var product = products.Find(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<Products> Post(Products newProduct)
+        {
+            products.Add(newProduct);
+            return CreatedAtAction(nameof(Get), new { id = newProduct.Id }, newProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var product = products.Find(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products.Remove(product);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, Products updatedProduct)
+        {
+            var product = products.Find(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product.Name = updatedProduct.Name;
+            product.Description = updatedProduct.Description;
+            product.Price = updatedProduct.Price;
+
+            return NoContent();
+        }
+    }
+}
---
+++ ApiTest1/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiTest1.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
